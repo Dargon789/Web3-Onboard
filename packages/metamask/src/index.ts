a823dbfe82aac417f9060de528aa9e2c7ecbb958
Thiendekaco <thienguo@gmail.com>
2023-11-08 16:16:09 +0700

    update library @subwallet_connect

--- packages/metamask/src/index.ts
+++ packages/metamask/src/index.ts
@@ -1,32 +1,6 @@
-import type { WalletInit } from '@web3-onboard/common'
-import type { MetaMaskSDK, MetaMaskSDKOptions } from '@metamask/sdk'
-import type { createEIP1193Provider } from '@web3-onboard/common'
-
-type ImportSDK = {
-  createEIP1193Provider: typeof createEIP1193Provider
-  MetaMaskSDKConstructor: typeof MetaMaskSDK
-}
-
-const loadImports = async () => {
-  if (importPromise) {
-    return await importPromise
-  }
-
-  const { createEIP1193Provider } = await import('@web3-onboard/common')
-  const importedSDK = await import('@metamask/sdk')
-  const MetaMaskSDKConstructor =
-    // @ts-ignore
-    importedSDK.MetaMaskSDK || importedSDK.default.MetaMaskSDK
-
-  if (!MetaMaskSDKConstructor) {
-    throw new Error('Error importing and initializing MetaMask SDK')
-  }
-
-  return { createEIP1193Provider, MetaMaskSDKConstructor }
-}
-
-let importPromise: Promise<ImportSDK> | null = null
-let sdk: MetaMaskSDK | null = null
+import type { MetaMaskSDKOptions } from '@metamask/sdk'
+import type { WalletInit } from '@subwallet_connect/common'
+export type { MetaMaskSDKOptions } from '@metamask/sdk'
 
 function metamask({
   options
@@ -34,53 +8,57 @@ function metamask({
   options: Partial<MetaMaskSDKOptions>
 }): WalletInit {
   return () => {
-    importPromise = loadImports().catch(error => {
-      throw error
-    })
-
     return {
       label: 'MetaMask',
       type : 'evm',
       getIcon: async () => (await import('./icon.js')).default,
       getInterface: async ({ appMetadata }) => {
-        sdk = (window as any).mmsdk || sdk // Prevent conflict with existing mmsdk instances
-
-        if (sdk) {
-          // Prevent re-initializing instance as it causes issues with MetaMask sdk mobile provider.
-          return {
-            provider: sdk.getProvider() as any,
-            instance: sdk
-          }
-        }
-
         const { name, icon } = appMetadata || {}
         const base64 = window.btoa(icon || '')
         const appLogoUrl = `data:image/svg+xml;base64,${base64}`
-        const imports = await importPromise
+        const { createEIP1193Provider } = await import('@subwallet_connect/common')
+        const { default: metaMask, MetaMaskSDK } = await import('@metamask/sdk')
 
-        if (!imports?.MetaMaskSDKConstructor) {
+        // Patch issue with MetaMask SDK, remove after SDK is fixed
+        localStorage.removeItem('providerType')
+
+        let MetaMaskSDKConstructor
+        if (!MetaMaskSDK) {
+          // @ts-ignore
+          MetaMaskSDKConstructor = metaMask.MetaMaskSDK
+        } else {
+          MetaMaskSDKConstructor = MetaMaskSDK
+        }
+
+        if (!MetaMaskSDKConstructor) {
           throw new Error('Error importing and initializing MetaMask SDK')
         }
 
-        const { MetaMaskSDKConstructor } = imports
-
-        sdk = new MetaMaskSDKConstructor({
+        const sdk = new MetaMaskSDKConstructor({
           ...options,
           dappMetadata: {
             name: options.dappMetadata?.name || name || '',
-            url: options.dappMetadata?.url || window.location.origin,
             base64Icon: appLogoUrl
           },
           _source: 'web3-onboard'
         })
-
         await sdk.init()
-        const provider = sdk.getProvider()
 
-        if (provider) {
-          ;(provider as any).disconnect = () => {
-            sdk?.terminate()
+        const getProvider = () => {
+          const provider = createEIP1193Provider(sdk.getProvider(), {})
+          provider.disconnect = () => {
+            sdk.terminate()
           }
+          return provider
+        }
+        const provider = getProvider()
+
+        const _request = provider.request
+        provider.request = async ({ method, params }) => {
+          if (sdk.isExtensionActive()) {
+            return (window.extension as any).request({ method, params })
+          }
+          return _request({ method, params }) as Promise<any>
         }
 
         return {
